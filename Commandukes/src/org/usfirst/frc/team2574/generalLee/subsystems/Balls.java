43bb877a4ccdc0a858c5b735f8c3e9236b2f8e52
2574 <clark.andreasen@gmail.com>
2016-02-17 21:11:34 -0600

    Added basic ballsucker code, currently commented out for testing other functons

---
+++ Commandukes/src/org/usfirst/frc/team2574/generalLee/subsystems/Balls.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team2574.generalLee.subsystems;
+
+import org.usfirst.frc.team2574.generalLee.RobotMap;
+import org.usfirst.frc.team2574.generalLee.commands.TeleBalls;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Balls extends Subsystem {
+    private static Talon ballRollerTal = new Talon(RobotMap.frontArmTalPWM);
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    public static void set(double speed) {
+    	ballRollerTal.set(speed);
+    }
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new TeleBalls());
+    }
+}
+
